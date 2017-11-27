0fb2919f4fd04faa37feeb37a62d4ae1bda98602
Cristian Aleman <cristianalemanfuentes@gmail.com>
2017-11-27 11:55:58 -0600

    Logica para anunciar mision y recibirla en los hijos

--- src/app/astronaut/astronaut.component.ts
+++ src/app/astronaut/astronaut.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { MissionService } from '../services/mission.service';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-astronaut',
@@ -6,10 +8,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./astronaut.component.css']
 })
 export class AstronautComponent implements OnInit {
+  @Input() astronaut: string;
+  mission = '<no mission announced>';
+  confirmed = false;
+  announced = false;
+  subscription: Subscription;
 
-  constructor() { }
+  constructor(private missionService: MissionService) {
+    this.subscription = missionService.missionAnnounced$.subscribe(mission => {
+      this.mission = mission;
+      this.announced = true;
+      this.confirmed = false;
+    });
+  }
 
   ngOnInit() {
+  }
+
+  confirm() {
+    this.confirmed = true;
+    this.missionService.confirmMission(this.astronaut);
   }
 
 }
--- src/app/missioncontrol/missioncontrol.component.ts
+++ src/app/missioncontrol/missioncontrol.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { MissionService } from '../services/mission.service';
 
 @Component({
   selector: 'app-missioncontrol',
   templateUrl: './missioncontrol.component.html',
-  styleUrls: ['./missioncontrol.component.css']
+  styleUrls: ['./missioncontrol.component.css'],
+  providers: [MissionService]
 })
 export class MissioncontrolComponent implements OnInit {
+  astronauts = ['Brandon', 'Cristian', 'Jesus'];
+  history: string[] = [];
 
-  constructor() { }
+  missions = ['Fly to the moon!', 'Fly to mars!', 'Fly to Vegas'];
+  nextMission = 0;
+
+  constructor(private missionService: MissionService) {
+    missionService.missionCondfirmed$.subscribe(astronaut => {
+      this.history.push(`${ astronaut } confirmed the mission `);
+    });
+
+  }
 
   ngOnInit() {
+  }
+
+
+  announce() {
+    const mission = this.missions[this.nextMission++];
+    this.missionService.announceMission(mission);
+    this.history.push(`Mission ${ mission } announced`);
+    if (this.nextMission >= this.missions.length) {
+      this.nextMission = 0;
+    }
   }
 
 }
